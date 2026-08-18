1e08960dba3a8da3c0c08474ea384973e889440c
philliphall <130250890+philliphall@users.noreply.github.com>
2026-08-18 02:11:16 -0800

    Add PeterDroge RP2040 Vial Keyboard

---
+++ keyboards/handwired/dactyl_manuform/peterdroge/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Full-duplex split communication
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+#define SERIAL_USART_DRIVER SIOD0
